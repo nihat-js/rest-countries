cb2ffdabe0768287003b1a62c3653c835344f1e7
nihat.js <abdullazadenihat@gmail.com>
2022-12-27 10:21:11 +0400

    added filter

--- src/App.jsx
+++ src/App.jsx
@@ -2,54 +2,72 @@ import { useEffect, useState } from 'react'
 import './App.scss'
 import axios from 'axios'
 function App() {
-   const [inpSearch,setInpSearch] = useState('')
-
-   const [mainData,setMainData] = useState([])
-   const [filteredData,setFilteredData] = useState([])
-   useEffect(()=>{
+   const [mainData, setMainData] = useState([])
+   const [filteredData, setFilteredData] = useState([])
+   const [filter, setFilter] = useState({})
+   useEffect(() => {
       axios.get('https://restcountries.com/v3.1/all')
-      .then (res => {
-         setMainData(res.data)
-         setFilteredData(res.data)
+         .then(res => {
+            setMainData(res.data)
+            setFilteredData(res.data)
+         })
+   }, [])
+
+
+
+   useEffect(() => {
+      const fd = mainData.filter(x => {
+         let foo1 = true;
+         let foo2 = true ;
+         if (filter.search) {
+            foo1 = x.name.common.toLowerCase().substring(0, filter.search.length) == filter.search.toLowerCase()
+         }
+         if (filter.region && filter.region != 'all'){
+            foo2 =  x.region.toLowerCase() == filter.region.toLowerCase()
+         }
+
+         return foo1 && foo2
+
       })
-   },[])
-   
-   const handleInput = (e) => {
-      setInpSearch(e.target.value)
-   }
 
-   useEffect(()=>{
-      console.log(inpSearch)
-      const fd = mainData.filter ( x => {
-         return x.name.common.toLowerCase().substring(0,inpSearch.length)  == inpSearch.toLowerCase()
-      }  )
       setFilteredData(fd)
-   },[inpSearch])
+   }, [filter])
 
-  return (
-    <div className='app'>
-      <div className="container">
-         <h2 className="heading">Where in the world</h2>
-         <input className='input-search' onChange={ (e) => handleInput(e) }  type="text" value={inpSearch}  />
-      </div>
-      <div className="container">
-            <div className="cards">
-            {filteredData.map( (item,index) => <Card key={index} data = {item} > </Card> )}
+   return (
+      <div className='app'>
+         <div className="container">
+            <h2 className="heading">Where in the world</h2>
+            <div className="filter">
+               <input className='input-search' onChange={(e) => setFilter({ ...filter, search: e.target.value })} type="text"  />
+               <select name="" id="" onChange={(e) => setFilter({...filter,region : e.target.value})   }>
+                  <option value="all"></option>
+                  <option value='africa' > Africa</option>
+                  <option value='americas' > Americas </option>
+                  <option value="asia"> Asia  </option>
+                  <option value="europe"> Europe </option>
+                  <option value="oceania"> Ocenia </option>
+
+               </select>
+            </div>
          </div>
-      </div>
+         <div className="container">
+            <div className="cards">
+               {filteredData.map((item, index) => <Card key={index} data={item} > </Card>)}
+            </div>
+         </div>
 
-    </div>
-  )
+      </div>
+   )
 }
 
 
-function Card (props){
+function Card(props) {
    return (
       <div className="card">
          <figure>
             <img src={props.data.flags.svg} alt="" />
          </figure>
-         <h3 className="heading"> {props.data.name.common } </h3>
+         <h3 className="heading"> {props.data.name.common} </h3>
          <div className="population">
             <h3 className="heading"> Population</h3>
             <span className='text'> {props.data.population} </span>
