53aa1a3829a80a3c2842791972f5e621bc9efd15
Nihat <abdullazadenihat@gmail.com>
2022-12-29 01:38:05 +0400

    added usereducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,90 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 import './App.scss'
 import axios from 'axios'
 function App() {
-   const [mainData, setMainData] = useState([])
-   const [filteredData, setFilteredData] = useState([])
-   const [filter, setFilter] = useState({})
-   useEffect(() => {
-      axios.get('https://restcountries.com/v3.1/all')
-         .then(res => {
-            setMainData(res.data)
-            setFilteredData(res.data)
-         })
-   }, [])
+  useEffect(() => {
+    async function  fetcher (){
+      let response = await axios.get('https://restcountries.com/v3.1/all')
+      dispatch({ type: 'setMainData', data: response.data })
+    }
+    fetcher()
+  },[])
 
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'setMainData':
+        return { ...state, mainData: action.data }
+      case 'setFilter':
+        return { ...state, filter: { ...state.filter, [action.key]: action.value } }
+    }
+  }
 
+  const [state, dispatch] = useReducer(reducer, {
+    mainData: [],
+    filter: { region : 'all' },
 
-   useEffect(() => {
-      const fd = mainData.filter(x => {
-         let foo1 = true;
-         let foo2 = true ;
-         if (filter.search) {
-            foo1 = x.name.common.toLowerCase().substring(0, filter.search.length) == filter.search.toLowerCase()
-         }
-         if (filter.region && filter.region != 'all'){
-            foo2 =  x.region.toLowerCase() == filter.region.toLowerCase()
-         }
+  })
 
-         return foo1 && foo2
+  useEffect(() => {
+    console.log('rendered')
+  }, [state.filter])
 
-      })
+  return (
+    <div className='app'>
+      <div className="container">
+        <h2 className="heading" > </h2>
+        <div className="filter">
+          <input className='input-search' onChange={(e) => dispatch({ type: 'setFilter', key: 'search', value: e.target.value })} type="text" />
+          <select name="" id="" onChange={(e) => dispatch({ type: 'setFilter', key: 'region', value: e.target.value })}>
+            <option value="all"></option>
+            <option value='africa' > Africa</option>
+            <option value='americas' > Americas </option>
+            <option value="asia"> Asia  </option>
+            <option value="europe"> Europe </option>
+            <option value="oceania"> Ocenia </option>
 
-      setFilteredData(fd)
-   }, [filter])
-
-   return (
-      <div className='app'>
-         <div className="container">
-            <h2 className="heading">Where in the world</h2>
-            <div className="filter">
-               <input className='input-search' onChange={(e) => setFilter({ ...filter, search: e.target.value })} type="text"  />
-               <select name="" id="" onChange={(e) => setFilter({...filter,region : e.target.value})   }>
-                  <option value="all"></option>
-                  <option value='africa' > Africa</option>
-                  <option value='americas' > Americas </option>
-                  <option value="asia"> Asia  </option>
-                  <option value="europe"> Europe </option>
-                  <option value="oceania"> Ocenia </option>
-
-               </select>
-            </div>
-         </div>
-         <div className="container">
-            <div className="cards">
-               {filteredData.map((item, index) => <Card key={index} data={item} > </Card>)}
-            </div>
-         </div>
-
+          </select>
+        </div>
       </div>
-   )
+      <div className="container">
+        <div className="cards">
+          {state.mainData.map((item, index) => {
+            if (item.name.common.toLowerCase().includes(state.filter.search || '' )  &&   ( state.filter.region == '' || state.filter.region == 'all' ? true :  state.filter.region == item.region  )    ){
+              return <Card key={index} data={item} > </Card>
+            }
+          } )}
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 
 function Card(props) {
-   return (
-      <div className="card">
-         <figure>
-            <img src={props.data.flags.svg} alt="" />
-         </figure>
-         <h3 className="heading"> {props.data.name.common} </h3>
-         <div className="population">
-            <h3 className="heading"> Population</h3>
-            <span className='text'> {props.data.population} </span>
-         </div>
-         <div className="region">
-            <h3 className="heading"> Region</h3>
-            <span className='text'> {props.data.region} </span>
-         </div>
-         <div className="capital">
-            <h3 className="heading"> Capital</h3>
-            <span className='text'> {props.data.capital} </span>
-         </div>
-
-
-
+  return (
+    <div className="card">
+      <figure>
+        <img src={props.data.flags.svg} alt="" />
+      </figure>
+      <h3 className="heading"> {props.data.name.common} </h3>
+      <div className="population">
+        <h3 className="heading"> Population</h3>
+        <span className='text'> {props.data.population} </span>
       </div>
-   )
+      <div className="region">
+        <h3 className="heading"> Region</h3>
+        <span className='text'> {props.data.region} </span>
+      </div>
+      <div className="capital">
+        <h3 className="heading"> Capital</h3>
+        <span className='text'> {props.data.capital} </span>
+      </div>
+
+
+
+    </div>
+  )
 }
 
 export default App
